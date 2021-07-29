130975b70db3fc6580c52f50291711f9d71f1826
juanitolpelucas <juancatalan2001@gmail.com>
2021-07-29 23:48:27 +0200

    Added different icons for Ligth and Dark mode

--- src/header.js
+++ src/header.js
@@ -1,15 +1,33 @@
-const changeTheme = () => {
-  const htmlClasses = document.querySelector('html').classList
-  if (localStorage.theme === 'dark') {
-    htmlClasses.remove('dark')
-    localStorage.removeItem('theme')
-  } else {
-    htmlClasses.add('dark')
-    localStorage.theme = 'dark'
-  }
-}
+import { useState } from 'react'
 
 function Header () {
+  const [darkMode, setdarkMode] = useState(false)
+
+  const Iconsvg = () => {
+    if (localStorage.theme !== 'dark') {
+      return (
+        <path fillRule='evenodd' d='M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z' />
+      )
+    } else {
+      return (
+        <path d='M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z' />
+      )
+    }
+  }
+
+  const changeTheme = () => {
+    const htmlClasses = document.querySelector('html').classList
+    if (localStorage.theme === 'dark') {
+      htmlClasses.remove('dark')
+      localStorage.removeItem('theme')
+      setdarkMode(false)
+    } else {
+      htmlClasses.add('dark')
+      localStorage.theme = 'dark'
+      setdarkMode(true)
+    }
+  }
+
   return (
 
     <nav className='bg-white shadow dark:bg-gray-800'>
@@ -24,7 +42,7 @@ function Header () {
             <div className='flex lg:hidden'>
               <button type='button' onClick={changeTheme} className='text-gray-500 dark:text-gray-200' aria-label='toggle menu'>
                 <svg viewBox='0 0 24 24' className='mt-3 w-8 h-8 fill-current'>
-                  <path fillRule='evenodd' d='M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z' />
+                  <Iconsvg />
                 </svg>
               </button>
             </div>
@@ -71,7 +89,7 @@ function Header () {
           </div>
           <button type='button' onClick={changeTheme} className='hidden lg:inline pl-4 text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none' aria-label='toggle menu'>
             <svg viewBox='0 0 24 24' className='mt-3 w-8 h-8 fill-current'>
-              <path fillRule='evenodd' d='M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z' />
+              <Iconsvg prop={darkMode} />
             </svg>
           </button>
         </div>
@@ -79,7 +97,5 @@ function Header () {
     </nav>
   )
 }
-
-  <button className='dark:text-white' onClick={changeTheme}>cambio de modo</button>
 
 export default Header
