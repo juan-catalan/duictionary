ce5df9703ac047e879b79f001fc02db27f81e6f0
juanitolpelucas <juancatalan2001@gmail.com>
2021-07-30 11:47:36 +0200

    Fixed navbar header

--- src/header.js
+++ src/header.js
@@ -38,25 +38,25 @@ function Header () {
   return (
 
     <nav className='bg-white shadow dark:bg-gray-800'>
-      <div className='container px-6 py-4 mx-auto lg:flex lg:justify-between lg:items-center'>
-        <div className='lg:flex lg:items-center'>
-          <div className='flex items-center justify-between'>
-            <div>
-              <a className='text-3xl font-bold text-gray-800 dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300' href=' '>dUIctionary</a>
-            </div>
-
-            {/* <!-- Mobile menu button --> */}
-            <div className='flex lg:hidden'>
-              <button type='button' onClick={changeTheme} className='text-gray-500 dark:text-gray-200' aria-label='toggle menu'>
-                <svg viewBox='0 0 24 24' className='mt-3 w-8 h-8 fill-current'>
-                  <Iconsvg />
-                </svg>
-              </button>
-            </div>
+      <div className='container px-6 py-4 mx-auto md:flex md:justify-between md:items-center'>
+        <div className='flex items-center justify-between'>
+          <div>
+            <a href=' ' className='text-3xl font-bold text-gray-800 dark:text-white hover:text-gray-700 dark:hover:text-gray-300 '>dUIctionary</a>
           </div>
 
-          {/* Hacer que este a la derecha */}
-          <div className='flex mt-6 lg:flex lg:mt-0 lg:-mx-2'>
+          {/* <!-- Mobile menu button --> */}
+          <div className='flex md:hidden'>
+            <button type='button' onClick={changeTheme} className='lg:inline pl-4 text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none' aria-label='toggle menu'>
+              <svg viewBox='0 0 24 24' className='mt-3 w-8 h-8 fill-current'>
+                <Iconsvg prop={darkMode} />
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        {/* <!-- Mobile Menu open: "block", Menu closed: "hidden" --> */}
+        <div className='items-center mt-6 lg:mt-0 md:flex'>
+          <div className='flex flex-row md:flex-row md:mx-6'>
             <a href=' ' className='mx-2 text-gray-600 dark:text-gray-300 hover:text-gray-500 dark:hover:text-gray-300' aria-label='Reddit'>
               <svg
                 className='w-5 h-5 fill-current' viewBox='0 0 24 24' fill='none'
@@ -92,13 +92,15 @@ function Header () {
                 />
               </svg>
             </a>
-
           </div>
-          <button type='button' onClick={changeTheme} className='hidden lg:inline pl-4 text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none' aria-label='toggle menu'>
-            <svg viewBox='0 0 24 24' className='mt-3 w-8 h-8 fill-current'>
-              <Iconsvg prop={darkMode} />
-            </svg>
-          </button>
+
+          <div className='flex justify-center md:block'>
+            <button type='button' onClick={changeTheme} className='hidden lg:inline pl-4 text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none' aria-label='toggle menu'>
+              <svg viewBox='0 0 24 24' className='mt-3 w-8 h-8 fill-current'>
+                <Iconsvg prop={darkMode} />
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
     </nav>
